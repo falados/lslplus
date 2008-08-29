13754f158b20dfec2b94f80a6877f13105c69654
Robert Greayer <robgreayer@users.sourceforge.net>
2008-08-29 02:25:07 +0000

    git-svn-id: https://lslplus.svn.sourceforge.net/svnroot/lslplus/trunk@146 bacdf5aa-2e47-0410-bb67-bee18bce2bb6

--- haskell/Lsl/SystemTester.hs
+++ haskell/Lsl/SystemTester.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS_GHC -XFlexibleContexts #-}
 module Lsl.SystemTester where
 
 import Control.Exception
--- haskell/Lsl/UnitTester.hs
+++ haskell/Lsl/UnitTester.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS_GHC -XFlexibleContexts #-}
 module Lsl.UnitTester where
 
 import Control.Exception
