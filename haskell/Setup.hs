a2ce5695c5689ec0711bfb345525f017b84c9d70
Robert Greayer <robgreayer@users.sourceforge.net>
2008-11-14 19:53:45 +0000

    git-svn-id: https://lslplus.svn.sourceforge.net/svnroot/lslplus/trunk@211 bacdf5aa-2e47-0410-bb67-bee18bce2bb6

--- haskell/Setup.hs
+++ haskell/Setup.hs
@@ -1,3 +1,10 @@
 import Distribution.Simple
-main = defaultMain
+import Distribution.Simple.Haddock (haddock, hscolour)
+import Distribution.PackageDescription
+
+main = defaultMainWithHooks (simpleUserHooks {
+    preHaddock = \a f -> do
+        print a
+        print f
+        return (Just $ emptyBuildInfo { hsSourceDirs = ["noqqsrc"]},[]) })
 
---
+++ haskell/noqqsrc/Language/Lsl/Internal/BuiltInModules.hs
@@ -0,0 +1,6 @@
+module Language.Lsl.Internal.BuiltInModules(avEventGen) where
+
+import Language.Lsl.Syntax
+
+avEventGen :: (String,LModule)
+avEventGen = ("$avEventGen", undefined)
