fa0c1d23b4da0c63cf978f86b37da5c19933f9d5
Robert Greayer <robgreayer@users.sourceforge.net>
2008-11-15 21:20:38 +0000

    git-svn-id: https://lslplus.svn.sourceforge.net/svnroot/lslplus/trunk@219 bacdf5aa-2e47-0410-bb67-bee18bce2bb6

--- haskell/src/LslPlus.hs
+++ haskell/src/LslPlus.hs
@@ -12,7 +12,7 @@ import IO
 import System
 import System.Exit
 
-version="0.1.1"
+version="0.1.3"
 usage progName = "Usage: " ++ progName ++ " [Version|MetaData|Compiler|ExpressionHandler|SimMetaData|SystemTester|UnitTester]"
 main = do
     progName <- getProgName
