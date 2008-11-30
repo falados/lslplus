df33ab5da8ce44bf5f536cef98dbca0ee5e20c3e
Robert Greayer <robgreayer@users.sourceforge.net>
2008-11-30 02:51:45 +0000

    git-svn-id: https://lslplus.svn.sourceforge.net/svnroot/lslplus/trunk@246 bacdf5aa-2e47-0410-bb67-bee18bce2bb6

--- eclipse/update-site/mksites.sh
+++ eclipse/update-site/mksites.sh
@@ -5,10 +5,15 @@ if [ "$#" != "1" ]; then
     exit 1
 fi
 
+
 for site in `awk '{print $1;}' siteurls.txt`
 do
     url=`grep "^$site " siteurls.txt | awk '{print $2;}'`
     desc=`grep "^$site " siteurls.txt | sed 's/^.*@//'`
     echo $desc
-    ant -DbaseUrl=$url -Dsite=$site -Ddesc="$desc" -Dversion=$1
+    if [ "$METADIST" = "" ]; then
+        ant -DbaseUrl=$url -Dsite=$site -Ddesc="$desc" -Dversion=$1
+    else
+        ant -DbaseUrl=$url -Dsite=$site -Ddesc="$desc" -Dversion=$1 -Dmeta-dist="$METADIST"
+    fi
 done
--- eclipse/update-site/xform-index.xsl
+++ eclipse/update-site/xform-index.xsl
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="ISO-8859-1"?>
 <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
 <xsl:param name="siteName"></xsl:param>
-<xsl:template match="site"><span>{$siteName}</span></xsl:template>
+<xsl:template match="site"><xsl:copy-of select="$siteName"/></xsl:template>
 <xsl:template match="@*|node()">
    <xsl:copy>
       <xsl:apply-templates select="@*|node()"/>
