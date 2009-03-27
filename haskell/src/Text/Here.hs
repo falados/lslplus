426901318afcf55a155e98f0a16bef3e59142761
Robert Greayer <robgreayer@users.sourceforge.net>
2009-03-27 01:44:05 +0000

    git-svn-id: https://lslplus.svn.sourceforge.net/svnroot/lslplus/trunk@345 bacdf5aa-2e47-0410-bb67-bee18bce2bb6

--- haskell/src/Text/Here.hs
+++ haskell/src/Text/Here.hs
@@ -18,10 +18,14 @@ import Data.Generics.Aliases(extQ)
 import qualified Language.Haskell.TH as TH
 import Language.Haskell.TH.Quote(QuasiQuoter(..),dataToPatQ,dataToExpQ)
 
+filt [] = []
+filt ('\r':'\n':cs) = '\n':filt cs
+filt (c:cs) = c:filt cs
+
 herePat :: String -> TH.Q TH.Pat
-herePat s = dataToPatQ (const Nothing) s
+herePat s = dataToPatQ (const Nothing) (filt s)
 hereExp :: String -> TH.Q TH.Exp
-hereExp s = dataToExpQ (const Nothing) s
+hereExp s = dataToExpQ (const Nothing) (filt s)
 
 -- | A quasi-quoter for a string...
 here :: QuasiQuoter
